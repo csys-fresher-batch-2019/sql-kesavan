121c7c85d962fc1d29d6cb80d44bfecb30ffa44d
Kesavan P <kesavanp555@gmail.com>
2019-12-27 17:39:09 +0530

    Create join3tables.sql

---
+++ join3tables.sql
@@ -0,0 +1,60 @@
+drop table books;
+create table books(
+book_id number primary key,
+book_name varchar2(50) not null,
+author_name varchar2(50) unique,
+price number not null,
+publisher varchar2(50) not null,
+version number not null
+);
+insert into books(book_id,book_name,author_name,price,publisher,version)
+values(1001,'Code complete','John',235,'Ajay publications',1);
+insert into books(book_id,book_name,author_name,price,publisher,version)
+values(1002,'Sql book','Michel',237,'Kumar publications',1);
+insert into books(book_id,book_name,author_name,price,publisher,version)
+values(1003,'Java complete','Rhonda',635,'Lakshmi publications',2);
+insert into books(book_id,book_name,author_name,price,publisher,version)
+values(1004,'Learn python','Ravi',675,'KPS publications',2);
+select * from books;
+
+
+drop table orders;
+create table orders(
+order_id number primary key,
+user_id number not null,
+book_id number not null,
+order_date timestamp not null,
+delivery_date timestamp not null,
+total_amount number not null check(total_amount>0),
+quantity number not null,
+status varchar2(20) default 'Not ordered',
+constraint book_id_fk foreign key(book_id) references books(book_id)
+);
+insert into orders(order_id,user_id,book_id,order_date,delivery_date,total_amount,quantity)
+values(21342,1231,1002,sysdate,sysdate+7,675,1);
+insert into orders(order_id,user_id,book_id,order_date,delivery_date,total_amount,quantity)
+values(21345,1341,1004,sysdate,sysdate+7,375,0);
+update orders
+set status='Ordered'
+where quantity>0;
+update orders
+set status='Not Ordered'
+where quantity<=0;
+select * from orders
+
+
+drop table stock;
+create table stock(
+order_id number not null,
+stock_value number,
+constraint order_id_fk foreign key(order_id) references orders(order_id)
+);
+insert into stock(order_id,stock_value)
+values(21348,12);
+insert into stock(order_id,stock_value)
+values(21342,10);
+select * from stock
+
+
+
+select * from books b,orders o,stock s where b.book_id=o.book_id and o.order_id=s.order_id;
